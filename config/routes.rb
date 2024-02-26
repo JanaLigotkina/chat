5f491c321cdcea551e4972aff18268d522cc66f0
Jana Ligotkina <janakolesnikova6293@gmail.com>
2024-02-26 15:57:20 +0400

    Add functional for message

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, contollers: { registrations: "registrations" }
+  resources :rooms, only: %i[create show], param: :title
+  resources :messages, only: :create
   root "rooms#index"
 end
